cf67bd66d2f9a5219ac261079ad64cbd442d2271
g23rom <cwlros@yandex.ru>
2021-08-03 06:58:25 +0800

    add new packege react-router-dom5.2.0, add page News, Music, Settings

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <NavBar />
-      
-      <div className = 'app-wrapper-content'>
-      <Dialogs />
-      </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <NavBar />
 
-      {/* <Profile /> */}
-    </div>
+        <div className='app-wrapper-content'>
+          <Route path="/Dialogs" component={Dialogs} />
+          <Route path="/Profile" component={Profile} />
+          <Route path="/News" component={News} />
+          <Route path="/Music" component={Music} />
+          <Route path="/Settings" component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
