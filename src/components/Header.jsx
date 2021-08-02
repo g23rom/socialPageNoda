80e04a554e9cbb019f581c4e888826e5a3ee1e6a
g23rom <cwlros@yandex.ru>
2021-08-02 16:02:05 +0800

    01_firstLoad

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <header className='header'>
+        <img className='logo' src='https://kartinkin.com/uploads/posts/2021-01/thumbs/1610501112_2-p-kruglii-fon-dlya-logotipa-3.png'></img>
+      </header>;
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+const Profile = () => {
+  return <div className='content'>
+    <div>
+      <img src='https://zastavok.net/ts/arhitektura/1452898870.jpg'></img>
+    </div>
+
+    <div>
+      ava + description
+      <img src='https://yt3.ggpht.com/a/AATXAJzBn0YrJpEiOmWr7hrzDJ4zr0ZhwHAPjZojzw=s900-c-k-c0xffffffff-no-rj-mo'></img>
+    </div>
+
+    <div>
+      MyPost
+
+      <div>
+        NewPost
+      </div>
+
+      <div>
+        post1
+      </div>
+
+      <div>
+        post2
+      </div>
+
+    </div>
+
+  </div>
+}
+
+export default Profile;
